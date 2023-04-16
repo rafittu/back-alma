c589a17409007ae57df8663731c3630b841dfc6e
rafittu <rafittu@outlook.com>
2023-04-16 19:25:07 -0300

    feat: update user repository function

--- src/modules/user/repository/user.repository.ts
+++ src/modules/user/repository/user.repository.ts
@@ -4,7 +4,11 @@ import * as bcrypt from 'bcrypt';
 import * as crypto from 'crypto';
 import { PrismaService } from '../../../prisma.service';
 import { IUserRepository } from '../structure/repository.structure';
-import { ICreateUser, PartialUser } from '../structure/service.structure';
+import {
+  ICreateUser,
+  IUpdateUser,
+  PartialUser,
+} from '../structure/service.structure';
 import { UserStatus } from '../structure/user-status.enum';
 import { AppError } from '../../../common/errors/Error';
 import { ipAddressToInteger } from '../../../modules/utils/helpers/user-module';
@@ -124,6 +128,76 @@ export class UserRepository implements IUserRepository<User> {
       return userResponse;
     } catch (error) {
       throw new AppError('user-repository.getUserById', 404, 'user not found');
+    }
+  }
+
+  async updateUser(data: IUpdateUser, userId: string) {
+    const salt = await bcrypt.genSalt();
+
+    const personalInfo = {
+      first_name: data.firstName,
+      last_name: data.lastName,
+      social_name: data.socialName,
+      born_date: data.bornDate,
+      mother_name: data.motherName,
+      status: data.status,
+    };
+
+    const contactInfo = {
+      username: data.username,
+      email: data.email,
+      phone: data.phone,
+    };
+
+    const securityInfo = {
+      password: await bcrypt.hash(data.password, salt),
+      salt,
+      confirmation_token: crypto.randomBytes(32).toString('hex'),
+      recover_token: null,
+    };
+
+    const userData = {
+      personal: {
+        update: personalInfo,
+      },
+      contact: {
+        update: contactInfo,
+      },
+      security: {
+        update: securityInfo,
+      },
+    };
+
+    try {
+      const user = await this.prisma.user.update({
+        data: userData,
+        where: {
+          id: userId,
+        },
+        include: {
+          contact: {
+            select: {
+              username: true,
+              email: true,
+            },
+          },
+          security: {
+            select: {
+              confirmation_token: true,
+            },
+          },
+        },
+      });
+
+      const { confirmation_token: confirmationToken } = user.security;
+      const userResponse = {
+        ...user,
+        security: { confirmationToken },
+      };
+
+      return userResponse;
+    } catch (error) {
+      throw new AppError('user-repository.updateUser', 500, 'user not updated');
     }
   }
 }
--- src/modules/user/structure/repository.structure.ts
+++ src/modules/user/structure/repository.structure.ts
@@ -1,7 +1,8 @@
-import { ICreateUser, PartialUser } from './service.structure';
+import { ICreateUser, IUpdateUser, PartialUser } from './service.structure';
 import { UserStatus } from './user-status.enum';
 
 export interface IUserRepository<User> {
   createUser(data: ICreateUser, status: UserStatus): Promise<User>;
   getUserById(id: string): Promise<PartialUser>;
+  updateUser(data: IUpdateUser, userId: string);
 }
