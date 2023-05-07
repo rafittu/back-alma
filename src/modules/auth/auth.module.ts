0179703949f2eaa2ee8f8196440b9cb900c56108
rafittu <rafittu@outlook.com>
2023-05-07 18:11:40 -0300

    import local strategy

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/prisma.service';
+import { LocalStrategy } from './infra/strategies/local.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [PrismaService, AuthService],
+  providers: [PrismaService, AuthService, LocalStrategy],
 })
 export class AuthModule {}
