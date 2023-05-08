0c4ca60c0771fbd42de4f5359812d27bc6ad3781
rafittu <rafittu@outlook.com>
2023-05-08 19:00:36 -0300

    feat: define public routes

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -10,12 +10,14 @@ import { LocalAuthGuard } from './infra/guards/local-auth.guard';
 import { HttpExceptionFilter } from 'src/common/filter/http-exception.filter';
 import { AppError } from 'src/common/errors/Error';
 import { AuthRequest, UserToken } from './structure/service.structure';
+import { isPublic } from './infra/decorators/is-public.decorator';
 
 @Controller('auth')
 @UseFilters(new HttpExceptionFilter(new AppError()))
 export class AuthController {
   constructor(private readonly signInService: SignInService) {}
 
+  @isPublic()
   @Post('/signin')
   @UseGuards(LocalAuthGuard)
   signIn(@Request() req: AuthRequest): UserToken {
