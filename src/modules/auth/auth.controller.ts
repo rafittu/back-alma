b87c3d01d2274bf7f35a1f6dc89b045581e89e22
rafittu <rafittu@outlook.com>
2023-05-08 14:25:56 -0300

    feat: import local auth guard

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post, Request } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { SignInService } from './services/signin.service';
+import { LocalAuthGuard } from './infra/guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly signInService: SignInService) {}
 
   @Post('/signin')
+  @UseGuards(LocalAuthGuard)
   signIn(@Request() req) {
     const { user } = req;
     return this.signInService.execute(user);
