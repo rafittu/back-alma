ac2441fdf6ac78e9446330a6014642d03f35d771
rafittu <rafittu@outlook.com>
2023-05-08 19:10:31 -0300

    feat: get me endpoint

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import {
   Controller,
+  Get,
   Post,
   Request,
   UseFilters,
@@ -9,8 +10,13 @@ import { SignInService } from './services/signin.service';
 import { LocalAuthGuard } from './infra/guards/local-auth.guard';
 import { HttpExceptionFilter } from 'src/common/filter/http-exception.filter';
 import { AppError } from 'src/common/errors/Error';
-import { AuthRequest, UserToken } from './structure/service.structure';
+import {
+  AuthRequest,
+  UserPayload,
+  UserToken,
+} from './structure/service.structure';
 import { isPublic } from './infra/decorators/is-public.decorator';
+import { CurrentUser } from './infra/decorators/current-user.decorator';
 
 @Controller('auth')
 @UseFilters(new HttpExceptionFilter(new AppError()))
@@ -23,5 +29,10 @@ export class AuthController {
   signIn(@Request() req: AuthRequest): UserToken {
     const { user } = req;
     return this.signInService.execute(user);
+  }
+
+  @Get('/me')
+  getMe(@CurrentUser() user: UserPayload) {
+    return user;
   }
 }
