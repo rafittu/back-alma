adeab894e20037a0ce16402b794a774427b905a2
rafittu <rafittu@outlook.com>
2023-05-24 15:38:01 -0300

    feat: set up user controller test

--- src/modules/user/test/user.controller.spec.ts
+++ src/modules/user/test/user.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from '../user.controller';
-import { UserService } from '../services/user.service';
+import { CreateUserService } from '../services/create-user.service';
+import { GetUserByIdService } from '../services/get-user-by-id.service';
+import { UpdateUserService } from '../services/update-user.service';
+import { DeleteUserService } from '../services/delete-user.service';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -8,7 +11,32 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        {
+          provide: CreateUserService,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+        {
+          provide: GetUserByIdService,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+        {
+          provide: UpdateUserService,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+        {
+          provide: DeleteUserService,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
