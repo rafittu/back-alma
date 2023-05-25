2471527af8c9744d645afd5355c4d7740cc07f7c
rafittu <rafittu@outlook.com>
2023-05-25 16:01:30 -0300

    test: set up user services test

--- src/modules/user/test/user.service.spec.ts
+++ src/modules/user/test/user.service.spec.ts
@@ -1,18 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from '../services/user.service';
+import { CreateUserService } from '../services/create-user.service';
+import { GetUserByIdService } from '../services/get-user-by-id.service';
+import { UpdateUserService } from '../services/update-user.service';
+import { DeleteUserService } from '../services/delete-user.service';
+import { UserRepository } from '../repository/user.repository';
+import { MailerService } from '@nestjs-modules/mailer';
 
-describe('UserService', () => {
-  let service: UserService;
+describe('User Services', () => {
+  let createUserService: CreateUserService;
+  let getUserByIdService: GetUserByIdService;
+  let updateUserService: UpdateUserService;
+  let deleteUserService: DeleteUserService;
+
+  let userRepository: UserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        CreateUserService,
+        GetUserByIdService,
+        UpdateUserService,
+        DeleteUserService,
+        {
+          provide: UserRepository,
+          useValue: {
+            formatPersonalInfo: jest.fn(),
+            formatContactInfo: jest.fn(),
+            formatSecurityInfo: jest.fn(),
+            formatUserResponse: jest.fn(),
+            createUser: jest.fn(),
+            getUserById: jest.fn(),
+            updateUser: jest.fn(),
+            deleteUser: jest.fn(),
+          },
+        },
+        {
+          provide: MailerService,
+          useValue: {
+            sendMail: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    createUserService = module.get<CreateUserService>(CreateUserService);
+    getUserByIdService = module.get<GetUserByIdService>(GetUserByIdService);
+    updateUserService = module.get<UpdateUserService>(UpdateUserService);
+    deleteUserService = module.get<DeleteUserService>(DeleteUserService);
+
+    userRepository = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(createUserService).toBeDefined();
+    expect(getUserByIdService).toBeDefined();
+    expect(updateUserService).toBeDefined();
+    expect(deleteUserService).toBeDefined();
   });
 });
