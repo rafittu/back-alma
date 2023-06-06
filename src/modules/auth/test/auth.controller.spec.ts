7e7290f25c3454d7ba9d8887438d9dc843d7acee
rafittu <rafittu@outlook.com>
2023-06-06 14:11:14 -0300

    test: setup auth module test

--- src/modules/auth/test/auth.controller.spec.ts
+++ src/modules/auth/test/auth.controller.spec.ts
@@ -1,17 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from '../auth.controller';
-import { AuthService } from '../services/signin.service';
+import { SignInService } from '../services/signin.service';
+import { ConfirmAccountEmailService } from '../services/confirm-email.service';
+import { RecoverPasswordService } from '../services/recover-password.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let signInService: SignInService;
+  let confirmAccountEmailService: ConfirmAccountEmailService;
+  let recoverPasswordService: RecoverPasswordService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        {
+          provide: SignInService,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+        {
+          provide: ConfirmAccountEmailService,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+        {
+          provide: RecoverPasswordService,
+          useValue: {
+            sendRecoverPasswordEmail: jest.fn(),
+            resetPassword: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    signInService = module.get<SignInService>(SignInService);
+    confirmAccountEmailService = module.get<ConfirmAccountEmailService>(
+      ConfirmAccountEmailService,
+    );
+    recoverPasswordService = module.get<RecoverPasswordService>(
+      RecoverPasswordService,
+    );
   });
 
   it('should be defined', () => {
