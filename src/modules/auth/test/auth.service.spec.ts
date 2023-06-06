47cd7ad4ea5b68f93b59e270733569fbdf5d6b25
rafittu <rafittu@outlook.com>
2023-06-06 16:17:28 -0300

    test: setup auth services test

--- src/modules/auth/test/auth.service.spec.ts
+++ src/modules/auth/test/auth.service.spec.ts
@@ -1,18 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from '../services/signin.service';
+import { ConfirmAccountEmailService } from '../services/confirm-email.service';
+import { RecoverPasswordService } from '../services/recover-password.service';
+import { SignInService } from '../services/signin.service';
+import { AuthRepository } from '../repository/auth.repository';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let signInService: SignInService;
+  let jwtService: JwtService;
+  let confirmAccountEmailService: ConfirmAccountEmailService;
+  let recoverPasswordService: RecoverPasswordService;
+
+  let authRepository: AuthRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        SignInService,
+        JwtService,
+        ConfirmAccountEmailService,
+        {
+          provide: RecoverPasswordService,
+          useValue: {
+            sendRecoverPasswordEmail: jest.fn(),
+
+            resetPassword: jest.fn(),
+          },
+        },
+        {
+          provide: AuthRepository,
+          useValue: {
+            validateUser: jest.fn(),
+            confirmAccountEmail: jest.fn(),
+            sendRecoverPasswordEmail: jest.fn(),
+            resetPassword: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authRepository = module.get<AuthRepository>(AuthRepository);
+    signInService = module.get<SignInService>(SignInService);
+    jwtService = module.get<JwtService>(JwtService);
+    confirmAccountEmailService = module.get<ConfirmAccountEmailService>(
+      ConfirmAccountEmailService,
+    );
+    recoverPasswordService = module.get<RecoverPasswordService>(
+      RecoverPasswordService,
+    );
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(signInService).toBeDefined();
+    expect(jwtService).toBeDefined();
+    expect(confirmAccountEmailService).toBeDefined();
+    expect(recoverPasswordService).toBeDefined();
   });
 });
