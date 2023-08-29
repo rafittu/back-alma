3407bc7bcfa0c80ab8a6e9ae1264a4d7d2b398b4
rafittu <rafittu@outlook.com>
2023-08-29 14:07:32 -0300

    feat: send email with confirmation token

--- src/modules/auth/services/resend-account-token.service.ts
+++ src/modules/auth/services/resend-account-token.service.ts
@@ -2,15 +2,36 @@ import { User } from '.prisma/client';
 import { Inject, Injectable } from '@nestjs/common';
 import { AuthRepository } from '../repository/auth.repository';
 import { IAuthRepository } from '../structure/auth-repository.structure';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class ResendAccountTokenEmailService {
   constructor(
     @Inject(AuthRepository)
     private authRepository: IAuthRepository<User>,
+    private mailerService: MailerService,
   ) {}
 
   async execute(id: string, email: string) {
-    return await this.authRepository.resendAccountToken(id, email);
+    const { confirmationToken } = await this.authRepository.resendAccountToken(
+      id,
+      email,
+    );
+
+    const confirmAccountEmail = {
+      to: email,
+      from: 'noreply@application.com',
+      subject: 'ALMA - Email de confirmação',
+      template: 'email-confirmation',
+      context: {
+        token: confirmationToken,
+      },
+    };
+
+    await this.mailerService.sendMail(confirmAccountEmail);
+
+    return {
+      message: 'account confirmation token resent',
+    };
   }
 }
