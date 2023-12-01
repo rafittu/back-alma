214e3a3baa38092d1f4bd8641c11571d29627bb0
rafittu <rafittu@outlook.com>
2023-12-01 16:30:44 -0300

    feat:confirm access to new channel

--- src/modules/auth/structure/auth-repository.structure.ts
+++ src/modules/auth/structure/auth-repository.structure.ts
@@ -1,12 +1,14 @@
 import { UserStatus } from 'src/modules/user/interfaces/user-status.enum';
 import { CredentialsDto } from '../dto/credentials.dto';
 import { UserPayload } from './service.structure';
+import { Channel } from '@prisma/client';
 
 export interface IAuthRepository<User> {
   validateUser(credentialsDto: CredentialsDto): Promise<UserPayload | User>;
   confirmAccountEmail(
     confirmationToken: string,
     status: UserStatus,
+    newChannel?: Channel,
   ): Promise<object>;
   sendRecoverPasswordEmail(email: string): Promise<string>;
   resetPassword(recoverToken: string, password: string): Promise<object>;
