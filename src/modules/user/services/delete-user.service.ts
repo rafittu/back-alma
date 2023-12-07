183c598f419fe14e38e2b2a2fb3d0939488eeac2
rafittu <rafittu@outlook.com>
2023-12-07 11:01:28 -0300

    feat: map user to return

--- src/modules/user/services/delete-user.service.ts
+++ src/modules/user/services/delete-user.service.ts
@@ -1,9 +1,13 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { UserRepository } from '../repository/user.repository';
-import { IUserRepository } from '../interfaces/repository.interface';
+import {
+  IUserRepository,
+  PrismaUser,
+} from '../interfaces/repository.interface';
 import { UserStatus } from '../interfaces/user-status.enum';
 import { User } from '@prisma/client';
 import { IUser } from '../interfaces/user.interface';
+import { mapUserToReturn } from 'src/modules/utils/helpers/helpers-user-module';
 
 @Injectable()
 export class DeleteUserService {
@@ -12,7 +16,16 @@ export class DeleteUserService {
     private userRepository: IUserRepository<User>,
   ) {}
 
+  private formatUserToReturn(user: PrismaUser): IUser {
+    return mapUserToReturn(user);
+  }
+
   async execute(userId: string): Promise<IUser> {
-    return await this.userRepository.deleteUser(userId, UserStatus.CANCELLED);
+    const user = await this.userRepository.deleteUser(
+      userId,
+      UserStatus.CANCELLED,
+    );
+
+    return this.formatUserToReturn(user);
   }
 }
