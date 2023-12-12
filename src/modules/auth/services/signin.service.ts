8dc75e67dafe4225562fb48bba0c6f22a5c9f341
rafittu <rafittu@outlook.com>
2023-12-12 17:38:25 -0300

    feat: validate user allowed channels service

--- src/modules/auth/services/signin.service.ts
+++ src/modules/auth/services/signin.service.ts
@@ -5,24 +5,35 @@ import {
   UserToken,
 } from '../structure/service.structure';
 import { JwtService } from '@nestjs/jwt';
+import { IAuthRepository } from '../structure/auth-repository.structure';
+import { Channel, User } from '@prisma/client';
 
 @Injectable()
 export class SignInService {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private authRepository: IAuthRepository<User>,
+  ) {}
 
-  execute(user: UserPayload, origin: string): UserToken {
-    console.log(origin);
+  async execute(user: UserPayload, origin: string): Promise<UserToken> {
+    const signInChannel = origin.toUpperCase() as Channel;
 
-    const payload: JtwPayload = {
-      sub: user.id,
-      username: user.username,
-      email: user.email,
-    };
+    try {
+      await this.authRepository.validateChannel(user.id, signInChannel);
 
-    const jwtToken = this.jwtService.sign(payload);
+      const payload: JtwPayload = {
+        sub: user.id,
+        username: user.username,
+        email: user.email,
+      };
 
-    return {
-      accessToken: jwtToken,
-    };
+      const jwtToken = this.jwtService.sign(payload);
+
+      return {
+        accessToken: jwtToken,
+      };
+    } catch (error) {
+      throw error;
+    }
   }
 }
