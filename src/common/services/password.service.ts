34347fc32d02643e4cc5d6288b3d47b4c8fd9213
rafittu <rafittu@outlook.com>
2023-12-22 10:45:36 -0300

    feat: function to generate expiration token

--- src/common/services/password.service.ts
+++ src/common/services/password.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import * as crypto from 'crypto';
+import { addMinutes, format } from 'date-fns';
 
 @Injectable()
 export class PasswordService {
@@ -20,7 +21,13 @@ export class PasswordService {
     return bcrypt.compare(password, hashedPassword);
   }
 
-  generateRandomToken(): string {
-    return crypto.randomBytes(32).toString('hex');
+  generateRandomToken(): { token: string; expiresAt: string } {
+    const token = crypto.randomBytes(32).toString('hex');
+
+    const currentDateTime = new Date();
+    const expiresAt = addMinutes(currentDateTime, 30);
+    const formattedExpiresAt = format(expiresAt, 'yyyy-MM-dd HH:mm:ss');
+
+    return { token, expiresAt: formattedExpiresAt };
   }
 }
