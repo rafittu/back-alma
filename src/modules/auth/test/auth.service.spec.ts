a86c5f4936f0f34a261ad639c7d92180baf70b72
rafittu <rafittu@outlook.com>
2023-12-22 17:39:36 -0300

    test: error case when ip address is invalid

--- src/modules/auth/test/auth.service.spec.ts
+++ src/modules/auth/test/auth.service.spec.ts
@@ -225,6 +225,21 @@ describe('AuthService', () => {
       expect(result).toEqual(response);
     });
 
+    it(`should throw an error if 'ipAddress' is invalid`, async () => {
+      const invalidIpAddress = 'invalid_ip_address';
+
+      try {
+        await confirmAccountEmailService.execute(
+          MockConfirmationToken,
+          invalidIpAddress,
+        );
+      } catch (error) {
+        expect(error).toBeInstanceOf(AppError);
+        expect(error.code).toBe(403);
+        expect(error.message).toBe('invalid ip address');
+      }
+    });
+
     it('should throw an error if token is expired or invalid', async () => {
       jest.spyOn(redisService, 'get').mockResolvedValueOnce(null);
 
@@ -275,6 +290,22 @@ describe('AuthService', () => {
 
       expect(authRepository.resetPassword).toHaveBeenCalledTimes(1);
       expect(result).toEqual(response);
+    });
+
+    it(`should throw an error if 'ipAddress' is invalid`, async () => {
+      const invalidIpAddress = 'invalid_ip_address';
+
+      try {
+        await recoverPasswordService.resetPassword(
+          MockConfirmationToken,
+          MockResetPassword,
+          invalidIpAddress,
+        );
+      } catch (error) {
+        expect(error).toBeInstanceOf(AppError);
+        expect(error.code).toBe(403);
+        expect(error.message).toBe('invalid ip address');
+      }
     });
 
     it('should throw an error if passwordConfirmation is incorrect', async () => {
