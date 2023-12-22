1f2ed4825e94f195d60dc6346f180263a26481d6
rafittu <rafittu@outlook.com>
2023-12-22 14:26:41 -0300

    feat: validate token before confirm email

--- src/modules/auth/services/confirm-email.service.ts
+++ src/modules/auth/services/confirm-email.service.ts
@@ -5,6 +5,8 @@ import { AuthRepository } from '../repository/auth.repository';
 import { IAuthRepository } from '../interfaces/auth-repository.interface';
 import { RedisCacheService } from '../../../common/redis/redis-cache.service';
 import { Channel } from '@prisma/client';
+import { PasswordService } from '../../../common/services/password.service';
+import { AppError } from '../../../common/errors/Error';
 
 @Injectable()
 export class ConfirmAccountEmailService {
@@ -12,17 +14,33 @@ export class ConfirmAccountEmailService {
     @Inject(AuthRepository)
     private authRepository: IAuthRepository<User>,
     private readonly redisCacheService: RedisCacheService,
+    private readonly passwordService: PasswordService,
   ) {}
 
   async execute(confirmationToken: string): Promise<object> {
-    const userCacheChannel = (await this.redisCacheService.get(
-      confirmationToken,
-    )) as Channel;
+    try {
+      const userCacheChannel = (await this.redisCacheService.get(
+        confirmationToken,
+      )) as Channel;
 
-    return await this.authRepository.confirmAccountEmail(
-      confirmationToken,
-      UserStatus.ACTIVE,
-      userCacheChannel,
-    );
+      const tokenExpiresAt =
+        await this.authRepository.findUserByToken(confirmationToken);
+
+      if (!this.passwordService.isTokenValid(tokenExpiresAt)) {
+        throw new AppError(
+          'auth-service.confirmEmail',
+          400,
+          'invalid or expired token',
+        );
+      }
+
+      return await this.authRepository.confirmAccountEmail(
+        confirmationToken,
+        UserStatus.ACTIVE,
+        userCacheChannel,
+      );
+    } catch (error) {
+      throw error;
+    }
   }
 }
