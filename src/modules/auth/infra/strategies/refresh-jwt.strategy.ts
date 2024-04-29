8d5b8ba838de5dc8023a330dc4d9dd592dd3d1c4
rafittu <rafittu@outlook.com>
2024-04-29 15:30:05 -0300

    feat: create refresh jwt strategy

---
+++ src/modules/auth/infra/strategies/refresh-jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { IJtwPayload, IUserPayload } from '../../interfaces/service.interface';
+
+@Injectable()
+export class RefreshJwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromBodyField('refresh'),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: IJtwPayload): Promise<IUserPayload> {
+    return {
+      id: payload.sub,
+      username: payload.username,
+      email: payload.email,
+      status: payload.status,
+    };
+  }
+}
